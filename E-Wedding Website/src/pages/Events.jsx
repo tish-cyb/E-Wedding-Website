f6371569f43afe7f57fb5d60a7b26ecbc6c42629
chelly <164139757+raechelly7@users.noreply.github.com>
2025-08-11 17:32:29 +0800

    Updated reception location

--- E-Wedding Website/src/pages/Events.jsx
+++ E-Wedding Website/src/pages/Events.jsx
@@ -10,14 +10,14 @@ function Events() {
           <h3>Ceremony</h3>
           <p>📅 Date: February 14, 2026</p>
           <p>🕒 Time: 3:00 PM</p>
-          <p>📍 Venue: St. Mary’s Church, New York</p>
+          <p>📍 Venue: St. Patrick’s Cathedral, New York</p>
         </div>
 
         <div className="event-item">
           <h3>Reception</h3>
           <p>📅 Date: February 14, 2026</p>
           <p>🕒 Time: 6:00 PM</p>
-          <p>📍 Venue: The Grand Ballroom, NYC Hotel</p>
+          <p>📍 Venue: Hyatt Grand Central, New York</p>
         </div>
       </div>
 
@@ -25,6 +25,14 @@ function Events() {
         <iframe
           title="Wedding Venue Location"
           src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d12088.620690419166!2d-73.98966880453197!3d40.758611699999996!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c258fc102f6e51%3A0x88603435e036aa5b!2sSt.%20Patrick&#39;s%20Cathedral!5e0!3m2!1sen!2sph!4v1754903005367!5m2!1sen!2sph"
+          allowFullScreen=""
+          loading="lazy"
+        ></iframe>
+      </div>
+      <div className="map-container">
+        <iframe
+          title="Wedding Reception Location"
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d12088.469861417292!2d-73.99269316173695!3d40.75944109539077!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c259022d712db1%3A0x500317cc3cc9f519!2sHyatt%20Grand%20Central%20New%20York!5e0!3m2!1sen!2sph!4v1754903706444!5m2!1sen!2sph"
           allowFullScreen=""
           loading="lazy"
         ></iframe>
