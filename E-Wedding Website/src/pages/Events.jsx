7ff8908712a60754339d58df1315c1a9ce3075ce
chelly <164139757+raechelly7@users.noreply.github.com>
2025-08-13 09:39:17 +0800

    Removed inline style

--- E-Wedding Website/src/pages/Events.jsx
+++ E-Wedding Website/src/pages/Events.jsx
@@ -58,7 +58,6 @@ function Events() {
         src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1931.2311608264456!2d120.97996215552124!3d14.515527604259752!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3397cdb9f25aa7b9%3A0x5a5d4fd070c78502!2sCrystal%20Pavilion%2C%20Okada%20Manila!5e0!3m2!1sen!2sph!4v1754991333390!5m2!1sen!2sph"
         width="100%"
         height="450"
-        style="border:0;"
         allowfullscreen=""
         loading="lazy">
       </iframe>
