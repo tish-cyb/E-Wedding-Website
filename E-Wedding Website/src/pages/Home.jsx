4917d8a1aaaf91c84047502cd46c5c70cc58f0d7
tish_cyb <tiffanygeneta@gmail.com>
2025-08-14 15:41:20 +0800

    tish

--- E-Wedding Website/src/pages/Home.jsx
+++ E-Wedding Website/src/pages/Home.jsx
@@ -4,8 +4,11 @@ import '../Styles/home.css';
 const Home = () => {
   return (
     <section className="home-hero" id="home">
-      <a href="#rsvp" className="hero-btn interactive-hover-btn">
-      </a>
+      <div className="hero-content">
+        <h1 className="hero-title">Belle Weddings</h1>
+        <p className="hero-subtitle">Bespoke Wedding Planners</p>
+        <a href="#rsvp" className="hero-btn">RSVP</a>
+      </div>
     </section>
   );
 };
