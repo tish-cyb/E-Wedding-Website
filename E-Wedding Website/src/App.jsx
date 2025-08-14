253e19cddea2214092a047deadd0eb29c20c7bbb
Jen Otehlie Mina <jenotehliemina@gmail.com>
2025-08-14 16:16:02 +0800

    Resolve merge conflict in App.jsx

--- E-Wedding Website/src/App.jsx
+++ E-Wedding Website/src/App.jsx
@@ -1,27 +1,20 @@
 import './App.css';
 import Navbar from './components/Navbar.jsx';
 import Home from './pages/Home.jsx';
-<<<<<<< Updated upstream
 
 import OurStory from './pages/OurStory.jsx';
 import Events from './pages/Events.jsx';
 
 import RSVP from './pages/RSVP.jsx';
-=======
-import OurStory from './pages/OurStory.jsx';
->>>>>>> Stashed changes
 
 function App() {
   return (
     <>
       <Navbar />
       <Home />
-<<<<<<< Updated upstream
       <Events />
       <RSVP />
-=======
       <OurStory />
->>>>>>> Stashed changes
     </>
   );
 }
